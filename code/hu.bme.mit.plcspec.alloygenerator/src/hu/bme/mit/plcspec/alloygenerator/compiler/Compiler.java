34a61baa2ea06bc15cbfdadc11fcbf9945dee127
Milán Unicsovics <u.milan@gmail.com>
2015-10-04 16:21:15 +0200

    Parse paths from generated coverage.

--- code/hu.bme.mit.plcspec.alloygenerator/src/hu/bme/mit/plcspec/alloygenerator/compiler/Compiler.java
+++ code/hu.bme.mit.plcspec.alloygenerator/src/hu/bme/mit/plcspec/alloygenerator/compiler/Compiler.java
@@ -1,12 +1,14 @@
 package hu.bme.mit.plcspec.alloygenerator.compiler;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import edu.mit.csail.sdg.alloy4.A4Reporter;
 import edu.mit.csail.sdg.alloy4.Err;
 import edu.mit.csail.sdg.alloy4.ErrorWarning;
 import edu.mit.csail.sdg.alloy4compiler.ast.Command;
 import edu.mit.csail.sdg.alloy4compiler.ast.Module;
+import edu.mit.csail.sdg.alloy4compiler.ast.Sig;
 import edu.mit.csail.sdg.alloy4compiler.parser.CompUtil;
 import edu.mit.csail.sdg.alloy4compiler.translator.A4Options;
 import edu.mit.csail.sdg.alloy4compiler.translator.A4Solution;
@@ -24,7 +26,6 @@ public class Compiler {
 	}
 
 	public void compile() {
-		System.out.println(System.getProperty("java.library.path"));
 		A4Reporter rep = new A4Reporter() {
             @Override public void warning(ErrorWarning msg) {
 //            	TODO Correct error handling
@@ -48,9 +49,16 @@ public class Compiler {
 	        }
 	        Command cmd = world.getAllCommands().get(0);
 	        A4Solution sol = TranslateAlloyToKodkod.execute_command(rep, world.getAllReachableSigs(), cmd, opt);
-	        System.out.println(sol.satisfiable());
 	        if (sol.satisfiable()) {
-	        	System.out.println(sol.getAllReachableSigs().toString());
+	        	ArrayList<Sig> paths = new ArrayList<Sig>();
+	        	for (Sig sig : sol.getAllReachableSigs()) {
+	        		System.out.println(sig.label);
+					if (sig.label.matches("this/Path")) paths.add(sig);
+				}
+	        	System.out.println(paths.get(0).getFields());
+	        } else {
+	        	// TODO Popup window with the error
+	        	System.err.println("The current model is unsatisfiable!");
 	        }
         } catch (Err e) {
 			// TODO Auto-generated catch block
