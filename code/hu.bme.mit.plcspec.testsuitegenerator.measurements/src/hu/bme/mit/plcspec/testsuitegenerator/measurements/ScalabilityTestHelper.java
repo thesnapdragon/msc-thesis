5b26a628ffa308b541ff197ffc57ddd803e785f2
Milán Unicsovics <u.milan@gmail.com>
2015-11-01 19:59:15 +0100

    FSMs generated for scalability tests.

--- code/hu.bme.mit.plcspec.testsuitegenerator.measurements/src/hu/bme/mit/plcspec/testsuitegenerator/measurements/ScalabilityTestHelper.java
+++ code/hu.bme.mit.plcspec.testsuitegenerator.measurements/src/hu/bme/mit/plcspec/testsuitegenerator/measurements/ScalabilityTestHelper.java
@@ -3,16 +3,9 @@ package hu.bme.mit.plcspec.testsuitegenerator.measurements;
 public class ScalabilityTestHelper {
 
 	public static void main(String[] args) {
-		String[] files = {
-				"scalability_test10",
-				"scalability_test100",
-				"scalability_test200",
-				"scalability_test400",
-				"scalability_test800"
-		};
 		PlchsmModelGenerator generator = new PlchsmModelGenerator();
-		for (String file : files) {
-			generator.generate(file);
+		for (int i = 2; i <= 50; i++) {
+			generator.generate("scalability_test_t0" + i);
 		}
 	}
 
